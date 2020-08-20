83870c13c34e87205789a44fb39c5b13c9b77bcd
ViniciusA97 <vinicius.queiroz@dce.ufpb.br>
2020-08-20 18:59:57 -0300

    Adicionando TypeOrm e rota de teste de configuraçõ do banco

--- src/database.ts
+++ src/database.ts
@@ -1,36 +1,7 @@
-require('dotenv').config();
-import Knex from 'knex';
-export class Database{
+import { createConnection } from 'typeorm';
 
-    private static instance:Database;
-    private db;
-
-    private constructor(){
-        this.buildKnex();
-    }
-
-    public static getInstance(): Database{
-        if(!this.instance){
-            this.instance = new Database();
-        }
-        return this.instance;
-    }
-
-    private buildKnex():void{
-    }
-
+function connect(){
+    createConnection();
 }
 
-export const config = {
-    client:process.env.DB_CLIENT,
-    conection:{
-        host: process.env.DB_HOST,
-        user:process.env.DB_USER,
-        password:process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-    },
-    migrations:{
-    }
-}
-
-const inst: Knex = Knex(config as Knex.Config);
+export { connect as connect}
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
-import { Database } from "./database";
+import { User } from './entities/User';
 
 const route = Router();
 
 route.get('/test', (request, response)=>{
     return response.status(200).json({"sucess":"true"}).send()
 });
+
+route.get('/testConnection', async (request, response)=>{
+    const userTest = new User();  
+    userTest.name= "teste";
+    userTest.email = "email";
+    userTest.password= "password";
+    await userTest.save();
+    return response.status(200).json({'id':userTest.id}).send();
+});
+
 
 export {route}
