5dbfae047ca1fdd1dadb58e6a0db76001ccf773c
lucianefalcao <lucianefalcaoff@gmail.com>
2020-08-20 07:08:03 -0300

    garantir que SignUpController retorna um erro se nenhum nome foi provido

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,7 +3,8 @@ import { Controllers } from "./controllers";
 export class SignUpController implements Controllers {
   handle(httpRequest: any): any {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Parâmetro faltando: nome')
     }
   }
 
